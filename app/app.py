ebdd5b4aa6d85c1977d3b5403d406e551761c91e
Hilton Magno <hdmagno@gmail.com>
2024-06-14 02:47:06 -0300

    validacoes de temperatura e rest client

--- app/app.py
+++ app/app.py
@@ -16,14 +16,26 @@ def index():
 def receive_temperature():
     global received_temperature
     data = request.json
+    
+    if data is None or 'temperature' not in data:
+        return jsonify({"error": "JSON com formato incorreto."}), 400
+    
     received_temperature = data.get('temperature')
+    
+    if not isinstance(received_temperature, (int, float)):
+        return jsonify({"error": "Valor de temperatura invalido."}), 400
+    
     logging.info(f"Temperatura recebida: {received_temperature}°C")
     socketio.emit('new_temperature', {'temperature': received_temperature})
+    
     return jsonify({"message": "Temperatura recebida com sucesso"}), 200
 
 @main.route('/temperature', methods=['GET'])
 def provide_temperature():
-    if received_temperature is not None:
-        return jsonify({"temperature": received_temperature}), 200
-    else:
-        return jsonify({"error": "Temperatura não disponível"}), 404
+    try:
+        if received_temperature is not None and isinstance(received_temperature, (int, float)):
+            return jsonify({"temperature": received_temperature}), 200
+        else:
+            return jsonify({"error": "Temperatura não disponivel"}), 404
+    except Exception as e:
+        return jsonify({"error": "Um erro ocorreu ao processar a requisicao."}), 500
